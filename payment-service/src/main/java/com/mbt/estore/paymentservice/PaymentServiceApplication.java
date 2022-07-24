951bc4c97347d0d6bc08e8ff4f5c4fcef337a505
Berker Turan <turanberker@yahoo.com>
2022-07-24 23:50:50 +0300

    small fixes

--- payment-service/src/main/java/com/mbt/estore/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/mbt/estore/paymentservice/PaymentServiceApplication.java
@@ -3,8 +3,10 @@ package com.mbt.estore.paymentservice;
 import com.mbt.estore.core.CoreAxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @Import({CoreAxonConfig.class})
 public class PaymentServiceApplication {
